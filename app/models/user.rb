68501692bd1bb25a4d108b4ecaffdadfc37d556d
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-11-23 00:51:37 +0100

    Update the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,43 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :validatable, :confirmable,
+
+  # Constants
+  PASSWORD_FORMAT = /\A
+    (?=.*\d)           # Must contain a digit
+    (?=.*[a-z])        # Must contain a lower case character
+    (?=.*[A-Z])        # Must contain an upper case character
+    (?=.*[[:^alnum:]]) # Must contain a symbol
+  /x
+
+  # Validations
+  validates :gender, inclusion: { in: %w[male female] }
+  validates :interest, inclusion: { in: %w[male female] }
+  validates :birthday, presence: true
+  validates :membership, inclusion: { in: %w[free premium VIP] }
+  validates :role, inclusion: { in: %w[user admin] }
+
+  # Email validation
+  validates :email,
+            format: { with: URI::MailTo::EMAIL_REGEXP },
+            presence: true,
+            uniqueness: { case_sensitive: false }
+
+  # Password validation
+  validates :password,
+            presence: true,
+            length: { within: Devise.password_length },
+            format: { with: PASSWORD_FORMAT },
+            if: :password_required?
+
+  # Custom validation for age
+  validate :must_be_at_least_18_years_old
+
+  private
+
+  # Custom validation for age
+  def must_be_at_least_18_years_old
+    return unless birthday.present? && birthday > 18.years.ago.to_date
+
+    errors.add(:birthday, "You must be at least 18 years old")
+  end
 end
