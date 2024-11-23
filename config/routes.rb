f1dd850b086578f0f9c050d706d2dc790701db5a
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-11-23 12:37:19 +0100

    Create setting, profile, msg and notification controllers with views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get "notifications/index"
+  get "profiles/show"
+  get "profiles/edit"
+  get "profiles/update"
+  get "messages/index"
+  get "messages/show"
+  get "messages/create"
+  get "settings/edit"
+  get "settings/update"
   devise_for :users
 
   # Defines the root path route ("/")
